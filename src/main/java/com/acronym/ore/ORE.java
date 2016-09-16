f16ab5bdbd5a2e27d1cb80cc6316e8e6f19452e6
Jared <jaredlll08@gmail.com>
2016-09-17 00:23:04 +0200

    order of things

--- src/main/java/com/acronym/ore/ORE.java
+++ src/main/java/com/acronym/ore/ORE.java
@@ -9,6 +9,7 @@ import com.acronym.ore.common.generators.feature.retro.RetroGenFlatBedrock;
 import com.acronym.ore.common.generators.retrogen.RetroGen;
 import com.acronym.ore.common.reference.Reference;
 import com.acronym.ore.proxy.CommonProxy;
+import com.ewyboy.worldstripper.common.network.PacketHandler;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
@@ -35,6 +36,9 @@ public class ORE {
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
         PROXY.initEngines();
+        GenerationRegistry.registerWorldGenerator("ore", WorldGenOreMinable.class);
+        GenerationRegistry.registerWorldGenerator("vein", WorldGenOreVein.class);
+        GenerationRegistry.registerWorldGenerator("geode", WorldGenOreGeode.class);
         MinecraftForge.ORE_GEN_BUS.register(new VanillaOreDisabler());
         PacketHandler.registerMessages(MODID);
         PROXY.registerKeybindings();
@@ -44,16 +48,12 @@ public class ORE {
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
-        GenerationRegistry.registerWorldGenerator("ore", WorldGenOreMinable.class);
-        GenerationRegistry.registerWorldGenerator("vein", WorldGenOreVein.class);
-        GenerationRegistry.registerWorldGenerator("geode", WorldGenOreGeode.class);
-        MinecraftForge.ORE_GEN_BUS.register(new VanillaOreDisabler());
 
+        MinecraftForge.ORE_GEN_BUS.register(new VanillaOreDisabler());
         RetroGen.registerRetroGenerator(new RetroGenFlatBedrock());
         GameRegistry.registerWorldGenerator(new WorldGenFlatBedrock(), 0);
         GameRegistry.registerWorldGenerator(new OREWG(), 0);
-        Reference.CONFIG_DIR = new File(event.getSuggestedConfigurationFile().getParent(), File.separator + NAME + File.separator);
-        Config.load();
+
     }
 
     @Mod.EventHandler
