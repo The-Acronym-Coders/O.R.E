b0a0b1e0f82586a70929e8c81582b0e1cedee01d
Jared <jaredlll08@gmail.com>
2016-09-20 09:24:16 +0200

    fixed proxy

--- src/main/java/com/acronym/ore/proxy/ClientProxy.java
+++ src/main/java/com/acronym/ore/proxy/ClientProxy.java
@@ -2,4 +2,8 @@ package com.acronym.ore.proxy;
 
 public class ClientProxy extends CommonProxy {
 
+    @Override
+    public void registerKeybindings() {
+        super.registerKeybindings();
+    }
 }
