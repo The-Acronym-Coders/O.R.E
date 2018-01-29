3679b55bfda91cdcd1f27fa3813bb4d4547bc7e3
Lanse <simon_stalnabb@hotmail.com>
2018-01-29 17:38:47 +0100

    fuck you gradle

--- src/main/java/com/teamacronymcoders/ore/Ore.java
+++ src/main/java/com/teamacronymcoders/ore/Ore.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.teamacronymcoders.ore;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
@@ -7,12 +7,12 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = Ore.MODID, name = Ore.NAME, version = Ore.VERSION)
+public class Ore
 {
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
-    public static final String VERSION = "1.0";
+    public static final String MODID = "ore";
+    public static final String NAME = "ORE";
+    public static final String VERSION = "1.0.0";
 
     private static Logger logger;
 
